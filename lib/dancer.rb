e68203b26bae353bc94e02bde24c614be3a02300
Allan Farca <allan.farca@gmail.com>
2020-04-23 02:11:31 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,8 +3,8 @@ require_relative './dance_module.rb'
 class Dancer
   include Dance
   
-  include FancyDance::InstanceMethods
   extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
  
   attr_accessor :name
  
